804740a53aacffce63021ef570649c2439a3fbcc
Elsayed Mokdam <elsayedmokdam@gmail.com>
2026-03-31 16:55:29 +0200

    feat: remove TestSchema from schemas

--- src/schemas/TestSchema.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-
-export const TestSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string().min(6),
-  bio: z.string().optional(),
-  country: z.string().optional(),
-});

e61e8cfd659062f2bebe8395022ff34e6c10c85a
Elsayed Mokdam <elsayedmokdam@gmail.com>
2026-09-12 01:19:54 +0300

    fix: correct return value in getMyToken function to ensure proper token retrieval

--- src/services/utils/helpers/getMyToken.ts
+++ src/services/utils/helpers/getMyToken.ts
@@ -19,5 +19,5 @@ export async function getMyToken() {
 
   if (!decoded || !decoded.realToken) return null;
 
-  return String(decoded.realToken);
+  return decoded?.realToken as string ? decoded: null;
 }
